a97dee0b49c37afedba9d70b307ce1c22e3629c7
Adebayo Nathaniel <adebayonathaniel79@gmail.com>
2025-02-26 10:15:31 +0100

    updated

--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,5 @@
 <?php
-
+use Illuminate\Support\Facades\Artisan;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
@@ -13,4 +13,28 @@ Route::group(['prefix' => 'services'], function () {
 });
 Route::group(['prefix' => 'contact'], function () {
     Route::get('/', [App\Http\Controllers\ContactController::class, 'contactFunction'])->name('contact_page');
+});
+
+Route::get('/clear-cache', function () {
+    $exitCode = Artisan::call('config:clear');
+
+    return $exitCode;
+});
+Route::get('/clear-route', function () {
+    $exitCode = Artisan::call('route:clear');
+
+    return $exitCode;
+});
+
+Route::get('/clear-view', function () {
+    $exitCode = Artisan::call('view:clear');
+
+    return $exitCode;
+});
+
+Route::get('/clear-laravel-log-file', function () {
+    exec('rm -f ' . storage_path('logs/*.log'));
+    exec('rm -f ' . base_path('*.log'));
+
+    return 'Log file deleted';
 });
