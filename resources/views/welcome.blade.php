8385c764021e960a96dff39a996876cc56d98d12
Adebayo Nathaniel <adebayonathaniel79@gmail.com>
2025-02-26 08:21:03 +0100

    new chnages made

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -413,8 +413,8 @@
             </div>
 
             <section style="display: flex; flex-wrap: wrap; justify-content: center; gap: 10px; margin-top: -90px;">
-                <img src="{{ asset('assets/img/gallery/gallery11.jpg') }}" alt="Image 1" style="width: 30%; height: auto; object-fit: cover;">
                 <img src="{{ asset('assets/img/gallery/gallery8_new.jpg') }}" alt="Image 2" style="width: 30%; height: auto; object-fit: cover;">
+                <img src="{{ asset('assets/img/gallery/gallery11.jpg') }}" alt="Image 1" style="width: 30%; height: auto; object-fit: cover;">
                 <img src="{{ asset('assets/img/gallery/gallery1.jpg') }}" alt="Image 3" style="width: 30%; height: auto; object-fit: cover;">
                 <img src="{{ asset('assets/img/gallery/gallery12.jpg') }}" alt="Image 4" style="width: 30%; height: auto; object-fit: cover;">
                 <img src="{{ asset('assets/img/gallery/img15.avif') }}" alt="Image 4" style="width: 30%; height: auto; object-fit: cover;">
